eced943dccb605fe4698db52aec3f1ddaf2e932c
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-09-21 15:32:29 -0600

    it's an addition chart!

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styled from "styled-components";
 
-// const alive = true;
-// const dead = false;
+const alive = true;
+const dead = false;
 
 // const nowBuffer = [25][25];
 let test = [];
@@ -11,17 +12,34 @@ for (let i = 0; i <= 25; i++) {
 
 let nowBuffer = [];
 for (let i = 0; i < 25; i++) {
-  nowBuffer.push(i);
+  nowBuffer[i] = [];
+  for (let j = 0; j < 25; j++) {
+    nowBuffer[i][j] = i + j;
+  }
 }
 
 const GameGrid = () => {
   return (
-    <div>
-      {nowBuffer.map((number) => (
-        <div>{number}</div>
+    <>
+      {nowBuffer.map((row) => (
+        <Row>
+          {row.map((cell) => {
+            return <Cell>{cell}</Cell>;
+          })}
+        </Row>
       ))}
-    </div>
+    </>
   );
 };
 
 export default GameGrid;
+
+const Cell = styled.div`
+  width: 2rem;
+  border: 1px solid black;
+`;
+
+const Row = styled.div`
+  background-color: red;
+  display: flex;
+`;
