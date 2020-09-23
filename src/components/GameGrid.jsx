5b0fa3c45501f41146d4a12a50e8cbfee8a9e28c
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-09-23 00:01:42 -0600

    mostly working, life forms keep becoming eternal

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import { Button } from "@material-ui/core";
 import styled from "styled-components";
 import produce from "immer";
+
+const initialRows = 25;
+const initialCols = 25;
 
 let initialBuffer = [];
 for (let i = 0; i < 25; i++) {
@@ -15,10 +18,52 @@ const GameGrid = () => {
   // eslint-disable-next-line
   const [nowBuffer, setBuffer] = useState(initialBuffer);
   const [simulationActive, setSimulationActive] = useState(false);
+  const [rows, setRows] = useState(initialRows);
+  const [cols, setCols] = useState(initialCols);
 
-  const toggleSimulation = () => {
-    setSimulationActive(!simulationActive);
-  };
+  const simulationActiveRef = useRef(simulationActive);
+  simulationActiveRef.current = simulationActive;
+
+  const simulation = useCallback(() => {
+    if (!simulationActiveRef.current) return;
+
+    setBuffer((buffer) => {
+      return produce(buffer, (copy) => {
+        for (let i = 0; i < rows; i++) {
+          for (let j = 0; j < cols; j++) {
+            let neighbors = 0;
+            const checkNeighbors = (newI, newJ) => {
+              if (
+                i + newI >= 0 &&
+                i + newI < rows &&
+                j + newJ >= 0 &&
+                j + newJ < cols
+              ) {
+                if (copy[i + newI][j + newJ]) neighbors++;
+              }
+            };
+            checkNeighbors(0, 1);
+            checkNeighbors(0, -1);
+            checkNeighbors(-1, 0);
+            checkNeighbors(1, 0);
+            checkNeighbors(1, 1);
+            checkNeighbors(-1, -1);
+            checkNeighbors(-1, 1);
+            checkNeighbors(1, -1);
+
+            if (neighbors < 2 || neighbors > 3) {
+              copy[i][j] = false;
+            } else if (neighbors === 3) {
+              copy[i][j] = true;
+            }
+          }
+        }
+      });
+    });
+
+    console.log("Generation");
+    setTimeout(simulation, 1000);
+  }, [rows, cols]);
 
   return (
     <>
@@ -55,7 +100,16 @@ const GameGrid = () => {
       </div>
 
       <ButtonDiv>
-        <Button variant="contained" onClick={toggleSimulation}>
+        <Button
+          variant="contained"
+          onClick={() => {
+            setSimulationActive(!simulationActive);
+            if (!simulationActive) {
+              simulationActiveRef.current = true;
+              simulation();
+            }
+          }}
+        >
           {simulationActive ? "Stop" : "Start"}
         </Button>
       </ButtonDiv>
